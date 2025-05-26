c82aac9634fcf7360265b5a931c7270320012272
ceherrera-sym <ceherrera.symetria@gmail.com>
2025-05-26 14:24:56 -0600

    Primer Commit

---
+++ android/app/src/main/kotlin/com/example/jhp43/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jhp43
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Pantalla completa permanente
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TV4.3 Guanajuato',
+      debugShowCheckedModeBanner: false,
+      home: TV43Player(),
+    );
+  }
+}
+
+class TV43Player extends StatefulWidget {
+  @override
+  _TV43PlayerState createState() => _TV43PlayerState();
+}
+
+class _TV43PlayerState extends State<TV43Player> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://5f1af61612fb5.streamlock.net/tv43gto/smil:tv43gto.smil/playlist.m3u8',
+      )
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child:
+            _controller.value.isInitialized
+                ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+                : CircularProgressIndicator(color: Colors.white),
+      ),
+    );
+  }
+}
