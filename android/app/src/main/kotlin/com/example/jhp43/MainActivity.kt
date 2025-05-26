380da00542f9a1d5eeeb8c24dbd097637ba27c0d
ceherrera-sym <ceherrera.symetria@gmail.com>
2025-05-26 15:19:38 -0600

    Version 1

--- android/app/src/main/kotlin/com/example/jhp43/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/jhp43/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.jhp43
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Mantener pantalla despierta
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+}
